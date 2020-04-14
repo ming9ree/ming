d65f289d8271552beb0719962ac84d0786c9b9c8
ming9ree <63630002+ming9ree@users.noreply.github.com>
2020-04-14 17:00:09 +0900

    Add files via upload

---
+++ swing_c++_1.2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//사용자가 2개의 수를 입력하도록 함
+	int a, b;
+	cout << "First number>> ";
+	cin >> a;
+	cout << "Second number>> ";
+	cin >> b;
+
+	//결과 출력
+	cout << "==========<계산 결과>==========" << endl;
+
+	//덧셈 출력
+	cout << a << "+" << b << "=" << a+b << endl;
+	//뺄셈 출력
+	cout << a << "-" << b << "=" << a-b << endl;
+	//곱셈 출력
+	cout << a << "*" << b << "=" << a*b << endl;
+	//나눗셈 출력
+	cout << a << "/" << b << "=" << a/b << endl;
+
+	return 0;
+}
